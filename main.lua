6d8f02db8cf8f3dc9ce11635b675076083205c68
realkidhub <22010532@st.phenikaa-uni.edu.vn>
2026-08-26 13:06:12 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,4 @@
+pcall(function()
+    local url = game.GameId == 10200395747 and "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/GaG2.lua" or "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/BloxFruits.lua"
+    loadstring(game:HttpGet(url))()
+end)
