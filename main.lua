c8eb6083be369525b0e52e4271db97627de3e8ca
realkidhub <22010532@st.phenikaa-uni.edu.vn>
2026-09-04 23:51:26 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,13 @@
 pcall(function()
-    local url = game.GameId == 10200395747 and "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/GaG2.lua" or "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/BloxFruits.lua"
+    local url
+
+    if game.PlaceId == 107778070777162 then
+        url = "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/RealKidEgg.lua"
+    elseif game.GameId == 10200395747 then
+        url = "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/GaG2.lua"
+    else
+        url = "https://raw.githubusercontent.com/realkidhub/Games/refs/heads/main/BloxFruits.lua"
+    end
+
     loadstring(game:HttpGet(url))()
 end)
